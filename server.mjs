6492aca95250b1df2b2f4c0e1f90b72bf62ec14e
Mairajk <mairajkhan597@gmail.com>
2022-12-13 18:03:07 +0500

    working...1

--- server.mjs
+++ server.mjs
@@ -6,7 +6,9 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import mongoose from "mongoose";
 
+// mongodb+srv://MairajK:<password>@cluster0.sihvwcq.mongodb.net/?retryWrites=true&w=majority 
 
 let productSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -20,6 +22,9 @@ const productModel = mongoose.model('products', productSchema);
 const app = express();
 
 const port = process.env.PORT || 5001;
+
+const mongodbURI = process.env.mongodbURI || 'mongodb+srv://MairajK:workhardin@cluster0.sihvwcq.mongodb.net/estore?retryWrites=true&w=majority';
+
 
 app.use(cors());
 app.use(express.json());
@@ -193,3 +198,31 @@ app.use("*", express.static(path.join(__dirname, "./web/build/index.html")));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+
+
+mongoose.connect(mongodbURI);
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
